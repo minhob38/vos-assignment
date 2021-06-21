d7a338600d2ee0a90ab0db348a2b756f3b3f8f30
kwon, minho <minhob38@gmail.com>
2021-06-21 22:51:24 +0900

    Add koa logger

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const Koa = require("koa");
 const Router = require("koa-router");
+const logger = require('koa-logger')
 require("dotenv").config();
 
 const api = require("./src/api");
@@ -8,6 +9,8 @@ const app = new Koa();
 const router = new Router();
 
 const port = process.env.PORT || 3000;
+
+app.use(logger());
 
 router.use(api.routes());
 app.use(router.routes());
