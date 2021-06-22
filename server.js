1949cc50848b55aca31e8be398957ce6df9f25bb
kwon, minho <minhob38@gmail.com>
2021-06-23 01:10:15 +0900

    Add csv parse code

--- server.js
+++ server.js
@@ -18,3 +18,19 @@ app.use(router.routes());
 
 db.sequelize.sync().then((req) => console.log("model is synchronized with db"));
 app.listen(port, ()=> console.log(`server connection: port ${port}`));
+
+const iconv  = require("iconv-lite");
+const csv = require("csv-parser");
+const fs = require("fs");
+const path = require("path");
+const result = [];
+
+fs.createReadStream(path.join(__dirname, "/assets/test.csv"))
+  .pipe(iconv.decodeStream("EUC-KR"))
+  .pipe(csv({}))
+  .on("data",(data) => {
+    result.push({
+      "pnu": data["고유번호"],
+    })
+  })
+  .on("end", () => {console.log(result)})
